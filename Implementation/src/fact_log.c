9c74799010c83cb64b56de94fb8e58bf7057e569
99003746 <nitin.shetty1@ltts.com>
2021-03-12 12:05:08 +0530

    dffsf

--- Implementation/src/fact_log.c
+++ Implementation/src/fact_log.c
@@ -1,30 +1,34 @@
 #include"fact_log.h"
 
-          //Logarithm Product Function
+//Logarithm Product Function
 
 float product(float a, float b)
 {
     if(a==0)
     {
-     
       return 0;
     }
     else if(b==0)
     {
-      
-       return 0;
+      return 0;
+    }
+    else if(a<0)
+    {
+      return 0;
+    }
+    else if(b<0)
+    {
+      return 0;
     }
     else
     {
-          double result;
-          result=log10(a)+log10(b);
-           
-        return result;
+      double result;
+      result=log10(a)+log10(b);
+      return result;
     }
 }
 
                 //Logarithm Ratios Function
-
 float ratios(float a, float b) 
 {
     if(a==0)
@@ -37,13 +41,19 @@ float ratios(float a, float b)
     
      return 0;
     }
-    
-     else
+    else if(a<0)
+    {
+        return 0;
+    }
+    else if(b<0)
+    {
+        return 0;
+    }
+    else
     {
         double result;
         result=log10(a)-log10(b);
         return result;
-      
     }
 }
               //Factorial Function
@@ -54,26 +64,24 @@ int factorial(int n)
     
     if(n==1)
     {
-	
-       return 1;
-     
-    
+        return 1;
     }
     else if(n==0)
     {
         return 1;
     }
+    else if(n<0)
+    {
+        return 0;
+    }
     else
     {
-	
-    for(i=1; i<=n; i++)
+        for(i=1; i<=n; i++)
     {
         fact=fact*i;
     }
         return fact;
-        
     }
-
 }
                    
 
