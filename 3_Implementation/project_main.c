c13eec8de03d032b0525da23d7bfb489a8e4fd07
99003746 <nitin.shetty1@ltts.com>
2021-03-13 12:45:13 +0530

    hghugyu

---
+++ 3_Implementation/project_main.c
@@ -0,0 +1,21 @@
+#include"fact_log.h"
+void setUp(void){}
+void tearDown(void){}
+
+int main()
+{
+
+float product(float a, float b);
+float ratios(float a, float b) ;
+int factorial(int n);
+
+
+
+}  
+
+
+   
+
+
+
+
